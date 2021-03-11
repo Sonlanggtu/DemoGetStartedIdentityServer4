e42d8f52bb1d576046a86da9fa4f8a2ace8f7377
Son Dam Ngoc <Damngocson@outlook.com.vn>
2021-03-11 16:37:19 +0700

    IdentityServer4 + SqlServer

---
+++ IdentityMicroservice/DbInitializer.cs
@@ -0,0 +1,81 @@
+﻿using IdentityMicroservice.Data;
+using IdentityMicroservice.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityMicroservice
+{
+    public class DbInitializer
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly string AdminRoleName = "Admin";
+        private readonly string UserRoleName = "Member";
+
+        public DbInitializer(ApplicationDbContext context,
+          UserManager<ApplicationUser> userManager,
+          RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task Seed()
+        {
+            #region Quyền
+
+            if (!_roleManager.Roles.Any())
+            {
+                await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Id = AdminRoleName,
+                    Name = AdminRoleName,
+                    NormalizedName = AdminRoleName.ToUpper(),
+                });
+                await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Id = UserRoleName,
+                    Name = UserRoleName,
+                    NormalizedName = UserRoleName.ToUpper(),
+                });
+            }
+
+            #endregion Quyền
+
+            #region Người dùng
+
+            if (!_userManager.Users.Any())
+            {
+                var result = await _userManager.CreateAsync(new ApplicationUser
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = "admin",
+                    FirstName = "Quản trị",
+                    LastName = "1",
+                    Email = "info.international@gmail.com",
+                    AccessFailedCount = 0,
+                    EmailConfirmed = false,
+                    LockoutEnabled = false,
+                    PhoneNumberConfirmed = false,
+
+                    TwoFactorEnabled = false
+                }, "Admin@123");
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByNameAsync("admin");
+                    await _userManager.AddToRoleAsync(user, AdminRoleName);
+                }
+            }
+
+            #endregion Người dùng
+
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
